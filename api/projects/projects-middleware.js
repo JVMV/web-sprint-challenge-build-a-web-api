92e4ca311538ab62a11e03972907d486f9b13b13
Justin Abellera <justin.c.abellera@gmail.com>
2022-09-10 17:28:28 -0700

    project post finished

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,26 @@
 // add middlewares here related to projects
+const Projects = require('./projects-model')
+
+const validateId = async (req, res, next) => {
+    const { id } = req.params
+    const validate = await Projects.get(id)
+    if(validate === [] || !validate) {
+        res.status(404).json({message: `the id "${id}" is not valid`})
+    } else {
+        req.projects = validate
+        next()
+    }
+}
+
+const validatePost = async (req, res, next) => {
+    const { name, description } = req.body
+    if(!name || !description) {
+        res.status(400).json({message: `name and description is required`})
+    } else {
+        const newProject = await Projects.insert(req.body)
+        req.newProject = newProject
+        next()
+    }
+}
+
+module.exports = { validateId, validatePost }
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,27 @@
 // Write your "projects" router here!
+const express = require('express')
+
+const Projects = require('./projects-model')
+
+const { validateId, validatePost } = require('./projects-middleware')
+
+const router = express.Router()
+
+router.get('/', async (req, res) => {
+    const reqProjects = await Projects.get()
+    res.status(200).json(reqProjects)
+})
+
+router.get('/:id', validateId, (req, res) => {
+    res.status(200).json(req.projects)
+})
+
+router.post('/', validatePost, (req, res) => {
+    res.status(200).json(req.newProject)
+})
+
+
+
+
+
+module.exports = router
